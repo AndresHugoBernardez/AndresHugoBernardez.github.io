b553d1bd1ef87390a2744f0d2d93796e37b0ce48
Andrés Hugo Bernárdez <94204912+AndresHugoBernardez@users.noreply.github.com>
2024-11-16 19:03:53 -0300

    Update jsLanguagesLabels.js

--- jsAB/jsLanguagesLabels.js
+++ jsAB/jsLanguagesLabels.js
@@ -79,7 +79,7 @@ function spanishVersion()
     etiquetaMostrarTodo.innerHTML="Mostrar Todo";         
     etiquetaOcultarTodo.innerHTML="Ocultar Todo";         
     etiquetaLinkedin.innerHTML="Visite mi Linkedin haciendo click aquí";            
-    etiquetaPaginaEnContruccion2.innerHTML="* Última actualización: 30 de octubre de 2024";
+    etiquetaPaginaEnContruccion2.innerHTML="* Última actualización: 16 de noviembre de 2024";
 
     IdiomaActual="Spanish";
 
@@ -124,7 +124,7 @@ function englishVersion()
     etiquetaMostrarTodo.innerHTML="Show All";         
     etiquetaOcultarTodo.innerHTML="Hide All";         
     etiquetaLinkedin.innerHTML="Visit my Linkedin Profile by clicking here";            
-    etiquetaPaginaEnContruccion2.innerHTML="* Last updated: October 30th, 2024";
+    etiquetaPaginaEnContruccion2.innerHTML="* Last updated: November 16th, 2024";
 
     IdiomaActual="English"
     }
