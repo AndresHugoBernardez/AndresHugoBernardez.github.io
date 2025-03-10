959306848b47ac3c856e8cd1c59f58f21a8caecc
Andrés Hugo Bernárdez <94204912+AndresHugoBernardez@users.noreply.github.com>
2025-03-10 09:37:02 -0300

    Update jsAB.js

--- jsAB/jsAB.js
+++ jsAB/jsAB.js
@@ -97,7 +97,7 @@ window.addEventListener('load',(event)=>{
     MusicFlag=1;
 
    
-    setTimeout(function(){Player.style.position='absolute';triggerMusic();triggerMusic();setTimeout(Player.style.position='static',1000);}, 3000);
+    setTimeout(function(){Player.style.position='fixed';triggerMusic();triggerMusic();setTimeout(function(){Player.style.position='static';},1000);}, 3000);
            
             
         
